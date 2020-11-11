58f446a2f2fcfea1484dfa68482981eb7c51d1ee
Maksim Ustinov <maksimu@users.noreply.github.com>
2020-11-11 10:20:50 -0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -4,3 +4,9 @@
 # QuickLook plugin that lets you view text files without their own dedicated QuickLook plugin. 
 # https://github.com/whomwah/qlstephen
 brew cask install qlstephen
+
+# Tools
+brew install keka iina cyberduck atom 
+
+# Developer Tools
+brew install jq postman
